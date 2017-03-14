d056164dc3a23dfcb5fa75fdd5258bcdbb266b3e
Steven Confessore <steven.confessore@gmail.com>
2017-03-13 20:08:12 -0400

    initial commit

---
+++ Navigator/Engine/Manager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ZzukBot.Game.Statics;
+using ZzukBot.Objects;
+
+namespace Navigator.Engine
+{
+    public class Manager
+    {
+        private static Lazy<Manager> _instance = new Lazy<Manager>(() => new Manager());
+        public static Manager Instance => _instance.Value;
+        private bool _runBot = false;
+        public bool Start(Action parCallback)
+        {
+            if (!ObjectManager.Instance.IsIngame)
+            {
+                return _runBot = false;
+            }
+            if (_runBot)
+            {
+                return false;
+            }
+            var player = ObjectManager.Instance.Player;
+            var target = ObjectManager.Instance.Target;
+            Location curPoint = player.Position;
+            Location endPoint = target.Position;
+            var path = Navigation.Instance.CalculatePath(ObjectManager.Instance.Player.MapId, curPoint, endPoint,
+                true);
+            player.CtmTo(endPoint);
+            return true;
+        }
+        public void Stop()
+        {
+            return;
+        }
+    }
+}
---
+++ Navigator/Navigator.cs
@@ -0,0 +1,59 @@
+﻿using Navigator.Engine;
+using System;
+using ZzukBot.ExtensionFramework.Interfaces;
+
+namespace Navigator
+{
+    public class Navigator : IBotBase
+    {
+        string author = "krycess";
+        string name = "Navigator";
+        int version = 1;
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Version
+        {
+            get
+            {
+                return version;
+            }
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        public void PauseBotbase(Action onPauseCallback)
+        {
+            throw new NotImplementedException();
+        }
+        public bool ResumeBotbase()
+        {
+            throw new NotImplementedException();
+        }
+        public void ShowGui()
+        {
+            throw new NotImplementedException();
+        }
+        public bool Start(Action parStopCallback)
+        {
+            Manager.Instance.Start(parStopCallback);
+            return true;
+        }
+        public void Stop()
+        {
+            Manager.Instance.Stop();
+        }
+    }
+}
