a00e87b2ad6593dba3e898383c58d8ef678f5744
Aaron Scherer <aequasi@gmail.com>
2017-03-15 16:32:05 -0700

    Adding logic to convert XML files on the fly.

--- Navigator/Engine/ProfileLoader.cs
+++ Navigator/Engine/ProfileLoader.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Windows.Forms;
 using ZzukBot.Objects;
 using System.Collections.Generic;
+using System;
+using System.Net;
 
 namespace Navigator.Engine
 {
@@ -18,10 +20,43 @@ namespace Navigator.Engine
             DialogResult result = dialog.ShowDialog();
             if (result == DialogResult.OK)
             {
-                ProfileData = JsonConvert.DeserializeObject<ProfileData>((new StreamReader(dialog.FileName)).ReadToEnd());
+
+                ProfileData = JsonConvert.DeserializeObject<ProfileData>(GetJson(dialog));
 
                 Waypoints = ProfileData.Profile.Hotspots.Select(x => x.Location).ToList();
             }
+        }
+
+        private string GetJson(OpenFileDialog dialog)
+        {
+            string content = new StreamReader(dialog.FileName).ReadToEnd();
+            if (dialog.FileName.EndsWith(".xml"))
+            {
+                content = ConvertXmlToJson(dialog.FileName, content);
+            }
+
+            return content;
+        }
+
+        private string ConvertXmlToJson(string profileName, string content)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://profile-converter.herokuapp.com/xml/{profileName}");
+            byte[] bytes;
+            bytes = System.Text.Encoding.ASCII.GetBytes(content);
+            request.ContentType = "text/xml; encoding='utf-8'";
+            request.ContentLength = bytes.Length;
+            request.Method = "POST";
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bytes, 0, bytes.Length);
+            requestStream.Close();
+            HttpWebResponse response;
+            response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return new StreamReader(response.GetResponseStream()).ReadToEnd();
+            }
+
+            throw new Exception("Could not convert XML to JSON: " + response.ToString());
         }
     }
 }
--- Navigator/GUI/CMD.Designer.cs
+++ Navigator/GUI/CMD.Designer.cs
@@ -33,63 +33,64 @@ namespace Navigator.GUI
         /// </summary>
         private void InitializeComponent()
         {
-			this.StartButton = new System.Windows.Forms.Button();
-			this.StopButton = new System.Windows.Forms.Button();
-			this.LoadProfileButton = new System.Windows.Forms.Button();
-			this.LoadProfileOFD = new System.Windows.Forms.OpenFileDialog();
-			this.SuspendLayout();
-			// 
-			// StartButton
-			// 
-			this.StartButton.Location = new System.Drawing.Point(174, 348);
-			this.StartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.StartButton.Name = "StartButton";
-			this.StartButton.Size = new System.Drawing.Size(112, 35);
-			this.StartButton.TabIndex = 0;
-			this.StartButton.Text = "Start";
-			this.StartButton.UseVisualStyleBackColor = true;
-			this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
-			// 
-			// StopButton
-			// 
-			this.StopButton.Location = new System.Drawing.Point(296, 348);
-			this.StopButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.StopButton.Name = "StopButton";
-			this.StopButton.Size = new System.Drawing.Size(112, 35);
-			this.StopButton.TabIndex = 1;
-			this.StopButton.Text = "Stop";
-			this.StopButton.UseVisualStyleBackColor = true;
-			this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
-			// 
-			// LoadProfileButton
-			// 
-			this.LoadProfileButton.Location = new System.Drawing.Point(18, 348);
-			this.LoadProfileButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.LoadProfileButton.Name = "LoadProfileButton";
-			this.LoadProfileButton.Size = new System.Drawing.Size(112, 35);
-			this.LoadProfileButton.TabIndex = 2;
-			this.LoadProfileButton.Text = "Load Profile";
-			this.LoadProfileButton.UseVisualStyleBackColor = true;
-			this.LoadProfileButton.Click += new System.EventHandler(this.LoadProfileButton_Click);
-			// 
-			// LoadProfileOFD
-			// 
-			this.LoadProfileOFD.FileName = "LoadProfile";
-			this.LoadProfileOFD.FileOk += new System.ComponentModel.CancelEventHandler(this.LoadProfileOFD_FileOk);
-			// 
-			// CMD
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(426, 402);
-			this.Controls.Add(this.LoadProfileButton);
-			this.Controls.Add(this.StopButton);
-			this.Controls.Add(this.StartButton);
-			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.Name = "CMD";
-			this.Text = "GUI";
-			this.Load += new System.EventHandler(this.GUI_Load);
-			this.ResumeLayout(false);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.LoadProfileButton = new System.Windows.Forms.Button();
+            this.LoadProfileOFD = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(174, 348);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(112, 35);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(296, 348);
+            this.StopButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(112, 35);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // LoadProfileButton
+            // 
+            this.LoadProfileButton.Location = new System.Drawing.Point(18, 348);
+            this.LoadProfileButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LoadProfileButton.Name = "LoadProfileButton";
+            this.LoadProfileButton.Size = new System.Drawing.Size(112, 35);
+            this.LoadProfileButton.TabIndex = 2;
+            this.LoadProfileButton.Text = "Load Profile";
+            this.LoadProfileButton.UseVisualStyleBackColor = true;
+            this.LoadProfileButton.Click += new System.EventHandler(this.LoadProfileButton_Click);
+            // 
+            // LoadProfileOFD
+            // 
+            this.LoadProfileOFD.FileName = "LoadProfile";
+            this.LoadProfileOFD.Filter = "Profiles (*.json;*.xml)|*.json;*.xml";
+            this.LoadProfileOFD.FileOk += new System.ComponentModel.CancelEventHandler(this.LoadProfileOFD_FileOk);
+            // 
+            // CMD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 402);
+            this.Controls.Add(this.LoadProfileButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CMD";
+            this.Text = "GUI";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.ResumeLayout(false);
 
         }
 
